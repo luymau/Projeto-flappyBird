397a8d9fcc3eef7ff4a709c286a513504093dc2a
Mauricio Mateus <mauriciomateus_13@hotmail.com>
2020-07-08 11:06:47 -0300

    Create Bird

--- js/flappy.js
+++ js/flappy.js
@@ -72,10 +72,42 @@ function Barreiras(altura, largura, abertura, espaco, notificarPonto){
     }
 }
 
+function Passaro(alturaJogo){
+    let voando = false;
+    
 
-const barreiras = new Barreiras(700, 900, 200, 400)
+    this.elemento = novoElemento('img', 'passaro');
+    this.elemento.src = 'imgs/passaro.png';
+
+    this.getY = () => parseInt(this.elemento.style.bottom.split('px')[0]);
+    this.setY = y => this.elemento.style.bottom = `${y}px`;
+
+    window.onkeydown = e => voando = true; //Se qualquer tecla tiver pressionada, será setado true
+    window.onkeyup = e => voando = false;
+
+    this.animar = () => {
+        const novoY = this.getY() + (voando ? 8 : -5) //Quanto o passaro sobe ou cai
+        const alturaMaxima = alturaJogo - this.elemento.clientHeight;
+
+        if(novoY <= 0){
+            this.setY(0)
+        }else if (novoY >= alturaMaxima) {
+            this.setY(alturaMaxima);
+        }else {
+            this.setY(novoY);
+        }
+    }
+
+  this.setY(alturaJogo / 2);
+}
+
+/*const barreiras = new Barreiras(700, 1200, 200, 400)
+const passaro = new Passaro(700)
 const areaJogo = document.querySelector('[wm-flappy]')
+
+areaJogo.appendChild(passaro.elemento);
 barreiras.pares.forEach(par => areaJogo.appendChild(par.elemento))
-/*setInterval( () => {
+setInterval( () => {
     barreiras.animar();
+    passaro.animar();
 },20)*/
